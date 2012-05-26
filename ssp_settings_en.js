49bb54109a2930d8d7d9c959da9df476a64b949c
Nimrod S. Kerrett <unclezzzen@gmail.com>
2012-05-27 01:35:18 +0700

    Forgot the actual language file :s

---
+++ ssp_settings_en.js
@@ -0,0 +1,89 @@
+window.ssp_settings_en = {
+    'services' : {
+        'facebook' : {
+            'status'            : 'on',
+            'dummy_img'         : 'socialshareprivacy/images/dummy_facebook_en.png',
+            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Facebook can not track your visit here. If you decide to recommend this page on Facebook, you can activate the Facebook button. Facebook might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_fb_off'        : 'not connected to Facebook',
+            'txt_fb_on'         : 'connected to Facebook',
+            'perma_option'      : 'on',
+            'display_name'      : 'Facebook',
+            'referrer_track'    : '',
+            'language'          : 'en_US',
+            'action'            : 'recommend'
+        }, 
+        'twitter' : {
+            'status'            : 'on', 
+            'dummy_img'         : 'socialshareprivacy/images/dummy_twitter.png',
+            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Twitter can not track your visit here. If you decide to tweet about this page, you can activate the Twitter button. Twitter might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_twitter_off'   : 'not connected to Twitter',
+            'txt_twitter_on'    : 'connected to Twitter',
+            'perma_option'      : 'on',
+            'display_name'      : 'Twitter',
+            'referrer_track'    : '', 
+            'language'          : 'en'
+        },
+        'gplus' : {
+            'status'            : 'on',
+            'dummy_img'         : 'socialshareprivacy/images/dummy_gplus.png',
+            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Google+ can not track your visit here. If you decide to recommend this page on Google+, you can activate the Google+ button. Google+ might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_gplus_off'     : 'not connected to Google+',
+            'txt_gplus_on'      : 'connected to Google+',
+            'perma_option'      : 'on',
+            'display_name'      : 'Google+',
+            'referrer_track'    : '',
+            'language'          : 'en'
+        }
+    },
+    'info_link'         : 'http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html',
+    'txt_help'          : 'If you activate these widgets, your visit here will be tracked by Facebook, Twitter and/or Google. Click for more information',
+    'settings_perma'    : 'Automatically enable widgets on this site for:',
+    'cookie_path'       : '/',
+    'cookie_expires'    : '365',
+    'css_path'          : 'socialshareprivacy/socialshareprivacy.css',
+};
+
+window.ssp_settings_en_like = {
+    'services' : {
+        'facebook' : {
+            'status'            : 'on',
+            'dummy_img'         : 'socialshareprivacy/images/dummy_facebook_en_like.png',
+            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Facebook can not track your visit here. If you decide to like this page on Facebook, you can activate the Facebook button. Facebook might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_fb_off'        : 'not connected to Facebook',
+            'txt_fb_on'         : 'connected to Facebook',
+            'perma_option'      : 'on',
+            'display_name'      : 'Facebook',
+            'referrer_track'    : '',
+            'language'          : 'en_US',
+            'action'            : 'like'
+        }, 
+        'twitter' : {
+            'status'            : 'on', 
+            'dummy_img'         : 'socialshareprivacy/images/dummy_twitter.png',
+            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Twitter can not track your visit here. If you decide to tweet about this page, you can activate the Twitter button. Twitter might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_twitter_off'   : 'not connected to Twitter',
+            'txt_twitter_on'    : 'connected to Twitter',
+            'perma_option'      : 'on',
+            'display_name'      : 'Twitter',
+            'referrer_track'    : '', 
+            'language'          : 'en'
+        },
+        'gplus' : {
+            'status'            : 'on',
+            'dummy_img'         : 'socialshareprivacy/images/dummy_gplus.png',
+            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Google+ can not track your visit here. If you decide to recommend this page on Google+, you can activate the Google+ button. Google+ might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_gplus_off'     : 'not connected to Google+',
+            'txt_gplus_on'      : 'connected to Google+',
+            'perma_option'      : 'on',
+            'display_name'      : 'Google+',
+            'referrer_track'    : '',
+            'language'          : 'en'
+        }
+    },
+    'info_link'         : 'http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html',
+    'txt_help'          : 'If you activate these widgets, your visit here will be tracked by Facebook, Twitter and/or Google. Click for more information',
+    'settings_perma'    : 'Automatically enable widgets on this site for:',
+    'cookie_path'       : '/',
+    'cookie_expires'    : '365',
+    'css_path'          : 'socialshareprivacy/socialshareprivacy.css',
+};
