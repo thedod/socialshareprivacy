c56e87ceeedc7ba3574f302456044ca1252ef0ab
Nimrod S. Kerrett <unclezzzen@gmail.com>
2012-05-27 01:51:19 +0700

    Text edits

--- ssp_settings_en.js
+++ ssp_settings_en.js
@@ -3,7 +3,7 @@ window.ssp_settings_en = {
         'facebook' : {
             'status'            : 'on',
             'dummy_img'         : 'socialshareprivacy/images/dummy_facebook_en.png',
-            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Facebook can not track your visit here. If you decide to recommend this page on Facebook, you can activate the Facebook button. Facebook might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_info'          : '2 clicks for better privacy:<br> As long as this button is off, Facebook can not track your visit here. If you decide to recommend this page on Facebook, you can activate the Facebook button. Facebook might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
             'txt_fb_off'        : 'not connected to Facebook',
             'txt_fb_on'         : 'connected to Facebook',
             'perma_option'      : 'on',
@@ -15,7 +15,7 @@ window.ssp_settings_en = {
         'twitter' : {
             'status'            : 'on', 
             'dummy_img'         : 'socialshareprivacy/images/dummy_twitter.png',
-            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Twitter can not track your visit here. If you decide to tweet about this page, you can activate the Twitter button. Twitter might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_info'          : '2 clicks for better privacy:<br> As long as this button is off, Twitter can not track your visit here. If you decide to tweet about this page, you can activate the Twitter button. Twitter might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
             'txt_twitter_off'   : 'not connected to Twitter',
             'txt_twitter_on'    : 'connected to Twitter',
             'perma_option'      : 'on',
@@ -26,7 +26,7 @@ window.ssp_settings_en = {
         'gplus' : {
             'status'            : 'on',
             'dummy_img'         : 'socialshareprivacy/images/dummy_gplus.png',
-            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Google+ can not track your visit here. If you decide to recommend this page on Google+, you can activate the Google+ button. Google+ might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_info'          : '2 clicks for better privacy:<br> As long as this button is off, Google+ can not track your visit here. If you decide to recommend this page on Google+, you can activate the Google+ button. Google+ might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
             'txt_gplus_off'     : 'not connected to Google+',
             'txt_gplus_on'      : 'connected to Google+',
             'perma_option'      : 'on',
@@ -36,7 +36,7 @@ window.ssp_settings_en = {
         }
     },
     'info_link'         : 'http://thedod.github.com/socialshareprivacy/about.html',
-    'txt_help'          : 'If you activate these widgets, your visit here will be tracked by Facebook, Twitter and/or Google. Click for more information',
+    'txt_help'          : 'If you activate these widgets, your visit here will be tracked by Facebook, Twitter and/or Google. Click on the <em>i</em> for more information',
     'settings_perma'    : 'Automatically enable widgets on this site for:',
     'cookie_path'       : '/',
     'cookie_expires'    : '365',
@@ -48,7 +48,7 @@ window.ssp_settings_en_like = {
         'facebook' : {
             'status'            : 'on',
             'dummy_img'         : 'socialshareprivacy/images/dummy_facebook_en_like.png',
-            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Facebook can not track your visit here. If you decide to like this page on Facebook, you can activate the Facebook button. Facebook might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_info'          : '2 clicks for better privacy:<br> As long as this button is off, Facebook can not track your visit here. If you decide to like this page on Facebook, you can activate the Facebook button. Facebook might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
             'txt_fb_off'        : 'not connected to Facebook',
             'txt_fb_on'         : 'connected to Facebook',
             'perma_option'      : 'on',
@@ -60,7 +60,7 @@ window.ssp_settings_en_like = {
         'twitter' : {
             'status'            : 'on', 
             'dummy_img'         : 'socialshareprivacy/images/dummy_twitter.png',
-            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Twitter can not track your visit here. If you decide to tweet about this page, you can activate the Twitter button. Twitter might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_info'          : '2 clicks for better privacy:<br> As long as this button is off, Twitter can not track your visit here. If you decide to tweet about this page, you can activate the Twitter button. Twitter might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
             'txt_twitter_off'   : 'not connected to Twitter',
             'txt_twitter_on'    : 'connected to Twitter',
             'perma_option'      : 'on',
@@ -71,7 +71,7 @@ window.ssp_settings_en_like = {
         'gplus' : {
             'status'            : 'on',
             'dummy_img'         : 'socialshareprivacy/images/dummy_gplus.png',
-            'txt_info'          : '2 clicks for greater privacy: As long as this button is off, Google+ can not track your visit here. If you decide to recommend this page on Google+, you can activate the Google+ button. Google+ might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
+            'txt_info'          : '2 clicks for better privacy:<br> As long as this button is off, Google+ can not track your visit here. If you decide to recommend this page on Google+, you can activate the Google+ button. Google+ might know about your visit, but you intend this to be public information anyway. For more informatin, click on the <em>i</em>',
             'txt_gplus_off'     : 'not connected to Google+',
             'txt_gplus_on'      : 'connected to Google+',
             'perma_option'      : 'on',
@@ -81,7 +81,7 @@ window.ssp_settings_en_like = {
         }
     },
     'info_link'         : 'http://thedod.github.com/socialshareprivacy/about.html',
-    'txt_help'          : 'If you activate these widgets, your visit here will be tracked by Facebook, Twitter and/or Google. Click for more information',
+    'txt_help'          : 'If you activate these widgets, your visit here will be tracked by Facebook, Twitter and/or Google. Click on the <em>i</em> for more information',
     'settings_perma'    : 'Automatically enable widgets on this site for:',
     'cookie_path'       : '/',
     'cookie_expires'    : '365',
